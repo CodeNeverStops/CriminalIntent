3b127923e05e5d4e09f47237537101e25a86e495
Wayne.You <lokiwuxi@gmail.com>
2016-10-13 13:32:33 +0800

    init project

---
+++ app/src/main/java/io/github/uv_lab/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package io.github.uv_lab.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by youwei on 2016/10/13.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
