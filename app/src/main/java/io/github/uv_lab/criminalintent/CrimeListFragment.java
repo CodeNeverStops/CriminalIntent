b723e52a87d53a16f9fdee493a51f6e71d0b33aa
Wayne.You <lokiwuxi@gmail.com>
2016-10-14 13:27:58 +0800

    start CrimeActivity

--- app/src/main/java/io/github/uv_lab/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/github/uv_lab/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package io.github.uv_lab.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -73,9 +74,9 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+//            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 
