7736ef84a26b06bd37004c5d4ddc97625b8e950c
Wayne.You <lokiwuxi@gmail.com>
2016-10-19 18:04:55 +0800

    add TimePicker Dialog

--- app/src/main/java/io/github/uv_lab/criminalintent/CrimeFragment.java
+++ app/src/main/java/io/github/uv_lab/criminalintent/CrimeFragment.java
@@ -26,12 +26,14 @@ public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+    private static final String DIALOG_TIME = "DialogTime";
 
     private static final int REQUEST_DATE = 0;
 
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
+    private Button mTimeButton;
     private CheckBox mSolvedCheckBox;
 
     public static CrimeFragment newInstance(UUID crimeId) {
@@ -86,6 +88,18 @@ public class CrimeFragment extends Fragment {
                         .newInstance(mCrime.getDate());
                 dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
+            }
+        });
+
+        mTimeButton = (Button) v.findViewById(R.id.crime_time);
+        mTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                TimePickerFragment dialog = TimePickerFragment
+                        .newInstance(mCrime.getDate());
+//                dialog.setTargetFragment(CrimeFragment.this, );
+                dialog.show(manager, DIALOG_TIME);
             }
         });
 
---
+++ app/src/main/java/io/github/uv_lab/criminalintent/TimePickerFragment.java
@@ -0,0 +1,48 @@
+package io.github.uv_lab.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Date;
+
+/**
+ * Created by youwei on 2016/10/19.
+ */
+
+public class TimePickerFragment extends DialogFragment {
+
+    private static final String ARG_TIME = "time";
+
+    private TimePicker mTimePicker;
+
+    public static TimePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TIME, date);
+
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_TIME);
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_time, null);
+
+        mTimePicker = (TimePicker) v.findViewById(R.id.dialog_time_time_picker);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.time_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+
+}
