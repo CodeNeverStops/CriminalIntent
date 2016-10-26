16eafd592063877460ecea0177890913a6cc8041
Wayne.You <lokiwuxi@gmail.com>
2016-10-26 17:06:48 +0800

    add menu

--- app/src/main/java/io/github/uv_lab/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/io/github/uv_lab/criminalintent/CrimePagerActivity.java
@@ -3,16 +3,16 @@ package io.github.uv_lab.criminalintent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID =
             "io.github.uv_lab.criminalintent.crime_id";
 
