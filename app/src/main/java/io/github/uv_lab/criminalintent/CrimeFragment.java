a19ae693f6273bc110f53355b9f879c387f8203c
Wayne.You <lokiwuxi@gmail.com>
2016-10-28 14:06:29 +0800

    add sqlite

--- app/src/main/java/io/github/uv_lab/criminalintent/CrimeFragment.java
+++ app/src/main/java/io/github/uv_lab/criminalintent/CrimeFragment.java
@@ -52,6 +52,14 @@ public class CrimeFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+
+        CrimeLab.get(getActivity())
+                .updateCrime(mCrime);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // create view of CrimeFragment
